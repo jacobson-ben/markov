d2f393123ecbe83ae35873104aab2011a21ff52f
Ben Jacobson <jacobson.ben11@gmail.com>
2021-03-23 19:09:46 -0400

    got markov code working

--- markov.js
+++ markov.js
@@ -8,6 +8,8 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     // MORE CODE HERE
+    this.chain = this.makeChains(words)
+    this.string = this.getText()
   }
 
   /** set markov chains:
@@ -15,8 +17,17 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // MORE CODE HERE
+  makeChains(words) {
+    let obj = {};
+    for(let i = 0; i < words.length; i++) {
+      if (obj[words[i]]) {
+        obj[words[i]].push(words[i+1])
+      }
+      else {
+        obj[words[i]] = [words[i+1]]
+      }
+    }
+    return obj
   }
 
 
@@ -24,5 +35,26 @@ class MarkovMachine {
 
   getText(numWords = 100) {
     // MORE CODE HERE
+    let startWordIdx = Math.floor(Math.random() * (Object.keys(this.chain).length));
+    let startWord = Object.keys(this.chain)[startWordIdx];
+    
+    let nextWordIdx = Math.floor(Math.random() * (this.chain[startWord].length))
+    let nextWord = this.chain[startWord][nextWordIdx]
+    
+    let string = startWord
+    let counter = 0
+    
+    while(counter < numWords) {
+      if(nextWord === undefined) {
+        break
+      }
+      string += ` ${nextWord}`;
+      startWord = nextWord
+      nextWordIdx = Math.floor(Math.random() * (this.chain[startWord].length))
+      nextWord = this.chain[startWord][nextWordIdx]
+      console.log("start word", startWord, "nextWord", nextWord)
+      counter++
+    }
+    return string
   }
 }
